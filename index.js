91db2fe709aef38245cf42c79a6bbcda99a998c6
unknown <harsith.rajkumar@gmail.com>
2022-07-15 17:22:46 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,5 @@
+const Prize = document.getElementById("Button_to_be_clicked");
+loginButton.addEventListener("click", (e) => {
+    e.preventDefault();
+    open("https://www.youtube.com/watch?v=dQw4w9WgXcQ")
+})
