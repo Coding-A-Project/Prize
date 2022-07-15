df2c2de267878e369502802d06f95d62ca0ead2f
unknown <harsith.rajkumar@gmail.com>
2022-07-15 17:34:02 +0530

    first commit

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 const Prize = document.getElementById("Button_to_be_clicked");
-loginButton.addEventListener("click", (e) => {
+Button_to_be_clicked.addEventListener("click", (e) => {
     e.preventDefault();
     open("https://www.youtube.com/watch?v=dQw4w9WgXcQ")
 })
